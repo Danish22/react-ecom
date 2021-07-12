82980416789bb5df5eec8ac9ebbb56138121a30a
Danish <danish.dewani@gmail.com>
2021-07-12 23:08:48 +0530

    added header

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-
+import Header from './components/header/header.component';
 
 function App() {
   return (
-    <div >
+    <div>
+    <Header />
+    
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route  path='/shop' component={ShopPage} />
       </Switch>
-    </div>
+      </div>
   );
 }
 
